af7b97c20d8ee1402b93652d9607e9f9540f6f51
gabby-grant <ggrant3@clemson.edu>
2025-05-13 13:05:14 -0500

    Update training-gene-set.sh

--- training-gene-set.sh
+++ training-gene-set.sh
@@ -13,19 +13,16 @@ module load anaconda3/2023.09-0
 
 
 #Go to working directory
-#cd /scratch/ffeltus/emt/GEMDIFF/BLCA
+cd /path/to/working/directory
 
 # activate the created conda environment
 source activate GEMDiff  #create before running script
 
 # clone the package
-#rm GEMDiff -rf
-#git clone https://github.com/xai990/GEMDiff.git
-#cd GEMDiff
+rm GEMDiff -rf
+git clone https://github.com/xai990/GEMDiff.git
+cd GEMDiff
 pip install -e . 
-
-#Move datasets to GEMDiff
-#cp ../datasets . -r
 
 #train the model
 python scripts/train.py --config data/UTERN-UCECT.yaml --dir log-0513 --gene_set genes-endo-cancer-paper.txt
