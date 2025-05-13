6c847c8984164d4bd232f621702b6b548ec39190
gabby-grant <ggrant3@clemson.edu>
2025-05-13 13:00:19 -0500

    Add files via upload

---
+++ training-gene-set-Copy1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --job-name=TRAIN     # Set the job name
+#SBATCH --nodes 1
+#SBATCH --tasks-per-node 1
+#SBATCH --cpus-per-task 4
+#SBATCH --gpus a100:1
+#SBATCH --mem 32gb
+#SBATCH --time 24:00:00
+
+#load modules
+module load anaconda3/2023.09-0
+
+
+#Go to working directory
+#cd /scratch/ffeltus/emt/GEMDIFF/BLCA
+
+# activate the created conda environment
+source activate GEMDiff  #create before running script
+
+# clone the package
+#rm GEMDiff -rf
+#git clone https://github.com/xai990/GEMDiff.git
+#cd GEMDiff
+pip install -e . 
+
+#Move datasets to GEMDiff
+#cp ../datasets . -r
+
+#train the model
+python scripts/train.py --config data/UTERN-UCECT.yaml --dir log-0513 --gene_set genes-endo-cancer-paper.txt
