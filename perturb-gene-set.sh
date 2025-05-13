a37ab8d6e8db2cf39f79275a7e7748a1bfe7a803
gabby-grant <ggrant3@clemson.edu>
2025-05-13 13:08:49 -0500

    Add files via upload

---
+++ perturb-gene-set.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=PERTURB      # Set the job name
+#SBATCH --nodes 1
+#SBATCH --tasks-per-node 1
+#SBATCH --cpus-per-task 4
+#SBATCH --gpus a100:1
+#SBATCH --mem 32gb
+#SBATCH --time 04:00:00
+
+#load modules
+module load anaconda3/2023.09-0
+
+
+#Go to working directory
+# cd gem-diff/GEMDiff/GEMDiff
+
+#Activate the created conda environment
+source activate GEMDiff  #create before running script
+
+#Perturb samples
+###Make sure to change the model path which is found in the train log file.###
+python scripts/perturb.py --config data/UTERN-UCECT.yaml --dir log-degs  --model_path log-degs/2025-05-08-21-16/model10000.pt --valid --gene_set top-degs-from-deseq.txt
